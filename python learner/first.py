2fdc518655214d3a81d11b8d7852797cc4b43978
Prathamesh karve <prathameshkarve19@gmail.com>
2026-03-28 11:04:25 +0530

    28 march ki subah ki filey

---
+++ python learner/first.py
@@ -0,0 +1,71 @@
+# # slicing   
+# toslice =  "happy"
+# print(toslice[0:3])
+
+# #modfiying strings
+# uper lower 
+# touse = "Prathuuu"
+# print(touse.upper())
+# print(touse.lower())
+
+# # removing whitespace : strip 
+# tostrip = "    gf   fhf  "
+# print(tostrip.strip())
+
+
+## repalcing letters
+# toreplace = "Prathuuu"
+# print(toreplace.replace('r','y'))
+
+## split the string into words  
+
+# tosplit = "Prathamesh karve "
+# print(tosplit.split())
+
+#### F string use (can you both f OR F)
+
+# toname = "prathuuu"
+# toage = 16
+# totext = F"my name is {toname} and my age is {toage}"
+# print(totext)
+# print(f"my name is age ois {toage}")
+# print(f"the calculation is {6+ 7}")
+
+# print("my name is \"Prathamesh\" ")
+# print("people also call him \"pinda\" ")
+
+
+# python string methods 
+
+# # capitalize 
+# tocap = "Prathumanesh"
+# print(tocap.capitalize())
+# print(tocap.casefold())
+# print(tocap.center(50))
+# print(tocap.count("P"))
+# print(tocap.find("t"))
+# print(tocap.index("thu"))
+
+
+# tobool = True 
+# print(tobool)
+
+# print(bool(""))
+# print(bool("hii"))
+# print(bool(455))
+# print(bool(0))
+# print(bool(-5))
+
+# ### is operator  and is not 
+# toapple = ["apple", "banana"]
+# tomapple  = ["apple", "banana"]
+# print(toapple is tomapple)
+# print(tomapple == tomapple)
+
+# print( tomapple is not toapple)
+
+# #### membership check with in and not in 
+# toary = ["aa" , "bb ", "cc"]
+# print("bb " in toary)
+# print("bb " not in toary)
+
