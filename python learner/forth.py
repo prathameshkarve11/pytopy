466c57c96a74bdf83dae382bcd02a50df3821546
Prathamesh karve <prathameshkarve19@gmail.com>
2026-03-29 09:19:53 +0530

    file handeling module added

---
+++ python learner/forth.py
@@ -0,0 +1,20 @@
+# # file with variable 
+# f = open("room1.txt" , "x+") 
+
+# f.close()
+
+# with open("room1.txt", "a+") as ff:
+#     ff.write(" this is next line after your first line \n")
+#     ff.seek(0)
+#     print(ff.read())
+
+
+
+# f2 = open("room2.txt" , "x")
+
+fr1 = open("room1.txt" , "r")
+fr1.seek(0)
+fr2 = open("room2.txt", "a")
+fr2.write(fr1.read())
+fr1.close()
+fr2.close()
