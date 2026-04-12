a7bb2801be343574c6485b26f2a04819dd002f13
Prathamesh karve <prathameshkarve19@gmail.com>
2026-04-12 12:11:10 +0530

    hi

--- python learner7.0/first7.0.py
+++ python learner7.0/first7.0.py
@@ -1,1 +1,42 @@
-print("helo")
+import pandas as pd
+
+dataset = {
+    "Names":["Raj", "Rushi", "Shivansh", "Prathamesh", "Anshuman"],
+    "Age":[21,20,20,19,None]
+}
+
+# s1 = pd.Series(dataset.__sizeof__())
+# print(s1)
+
+df = pd.DataFrame(dataset)
+
+
+# df.dropna(inplace= True)  
+# df.fillna(45 , inplace= True)
+# print(df)
+
+print(df["Names"].min())
+
+print(df[df["Age"] == 19])
+
+
+print(df[df["Age"] == 19])
+
+
+print(df[df["Age"] == 19])
+
+
+print(df[df["Age"] == 19])
+
+
+print(df[df["Age"] == 19])
+
+
+print(df[df["Age"] == 19])
+
+
+print(df[df["Age"] == 19])
+
+
+print(df[df["Age"] == 19])
+
